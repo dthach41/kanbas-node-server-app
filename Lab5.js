48345d48be23ab32e86b0ac5c0fe4df12980e688
dthach41 <thachderek1128@gmail.com>
2024-03-29 09:58:43 -0400

    Working on 3.2.4

--- Lab5.js
+++ Lab5.js
@@ -1,3 +1,16 @@
+const assignment = {
+    id: 1, title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10", completed: false, score: 0,
+};
+
+const module = {
+    id: 1, name: "Module Name",
+    description: "Module's Description",
+    course: "Module's Course"
+}
+
+
 const Lab5 = (app) => {
     app.get("/a5/welcome", (req, res) => {
         res.send("Welcome to Assignment 5");
@@ -12,5 +25,56 @@ const Lab5 = (app) => {
         const sum = parseInt(a) - parseInt(b);
         res.send(sum.toString());
     });
+    app.get("/a5/calculator", (req, res) => {
+        const { a, b, operation } = req.query;
+        let result = 0;
+        switch (operation) {
+            case "add":
+                result = parseInt(a) + parseInt(b);
+                break;
+            case "subtract":
+                result = parseInt(a) - parseInt(b);
+                break;
+            default:
+                result = "Invalid operation";
+        }
+        res.send(result.toString());
+    });
+
+    app.get("/a5/assignment", (req, res) => {
+        res.json(assignment);
+    });
+
+    app.get("/a5/assignment/title", (req, res) => {
+        res.json(assignment.title);
+    });
+
+    app.get("/a5/assignment/title/:newTitle", (req, res) => {
+        const { newTitle } = req.params;
+        assignment.title = newTitle;
+        res.json(assignment);
+    });
+
+    app.get("/a5/module", (req, res) => {
+        res.json(module);
+    })
+
+    app.get("/a5/module/name", (req, res) => {
+        res.json(module.name);
+    })
+
+    app.get("/a5/module/name/:newName", (req, res) => {
+        const { newName } = req.params;
+        module.name = newName;
+        res.json(module);
+    });
+
+    
+
+    app.get 
+
+
+
+
 };
 export default Lab5;

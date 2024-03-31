6d8d557b4a017c66503d8753fb0cbde997d13713
dthach41 <thachderek1128@gmail.com>
2024-03-30 20:08:40 -0400

    commit

--- Lab5.js
+++ Lab5.js
@@ -12,5 +12,35 @@ const Lab5 = (app) => {
         const sum = parseInt(a) - parseInt(b);
         res.send(sum.toString());
     });
+    app.get("/a5/calculator", (req, res) => {
+        const { a, b, operation } = req.query;
+        let result = 0;
+        switch (operation) {
+            case "add":
+                result = parseInt(a) + parseInt(b);
+                break;
+            case "subtract":
+                result = parseInt(a) - parseInt(b);
+                break;
+            default:
+                result = "Invalid operation";
+        }
+        res.send(result.toString());
+    });
+
+    app.get("/a5/assignment", (req, res) => {
+        res.json(assignment);
+    });
+
+    const assignment = {
+        id: 1, title: "NodeJS Assignment",
+        description: "Create a NodeJS server with ExpressJS",
+        due: "2021-10-10", completed: false, score: 0,
+    };
+
+
+
 };
+
+
 export default Lab5;

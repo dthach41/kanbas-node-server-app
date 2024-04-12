bd3182114160b6e57671922df057839abd6a4ec7
dthach41 <thachderek1128@gmail.com>
2024-04-11 22:26:59 -0400

    finish 4.3

--- App.js
+++ App.js
@@ -4,16 +4,20 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
+import mongoose from 'mongoose';
+import UserRoutes from "./Users/routes.js";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 
 Hello(app);
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 
-app.listen(4000)
+app.listen(process.env.PORT || 4000);
 

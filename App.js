49e67d96eccdadb17222ef03c8acb18e64fa04f3
dthach41 <thachderek1128@gmail.com>
2024-04-15 14:42:05 -0400

    a6 last part broken

--- App.js
+++ App.js
@@ -8,9 +8,36 @@ import mongoose from 'mongoose';
 import UserRoutes from "./Users/routes.js";
 import session from "express-session";
 import "dotenv/config";
+import { MongoClient, ServerApiVersion } from "mongodb"
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
+
+const client = new MongoClient(CONNECTION_STRING, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+console.log(CONNECTION_STRING)
 
 app.use(
     cors({

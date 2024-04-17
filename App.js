bdd9f53250199ca0f598566e98888bc8827f76a3
dthach41 <thachderek1128@gmail.com>
2024-04-17 11:43:07 -0400

    f

--- App.js
+++ App.js
@@ -10,32 +10,10 @@ import session from "express-session";
 import "dotenv/config";
 import { MongoClient, ServerApiVersion } from "mongodb"
 
-const CONNECTION_STRING =  "mongodb://127.0.0.1:27017/kanbas"
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-
-const client = new MongoClient(CONNECTION_STRING, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-run().catch(console.dir);
 
 console.log(CONNECTION_STRING)
 
@@ -43,8 +21,11 @@ app.use(
     cors({
         credentials: true,
         origin: process.env.FRONTEND_URL
+
     })
 );
+
+
 const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,

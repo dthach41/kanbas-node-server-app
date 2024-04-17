707546c928b2e8565f8cb76a966e4f2345962aee
dthach41 <thachderek1128@gmail.com>
2024-04-16 21:31:39 -0400

    f

--- Kanbas/modules/routes.js
+++ Kanbas/modules/routes.js
@@ -3,40 +3,8 @@ function ModuleRoutes(app) {
     app.get("/api/courses/:cid/modules", (req, res) => {
         const { cid } = req.params;
         const modules = db.modules
-            .filter((module) => module.courseId === cid);
+            .filter((m) => m.course === cid);
         res.send(modules);
     });
-
-    app.post("/api/courses/:cid/modules", (req, res) => {
-        const { cid } = req.params;
-        const newModule = {
-            ...req.body,
-            courseId: cid,
-            _id: new Date().getTime().toString(),
-        };
-        db.modules.push(newModule);
-        res.send(newModule);
-    });
-
-
-    app.delete("/api/modules/:mid", (req, res) => {
-        const { mid } = req.params;
-        db.modules = db.modules.filter((m) => m._id !== mid);
-        res.sendStatus(200);
-    });
-
-    app.put("/api/modules/:mid", (req, res) => {
-        const { mid } = req.params;
-        const moduleIndex = db.modules.findIndex(
-            (m) => m._id === mid);
-        db.modules[moduleIndex] = {
-            ...db.modules[moduleIndex],
-            ...req.body
-        };
-        res.sendStatus(204);
-    });
-
-
-
 }
 export default ModuleRoutes;
